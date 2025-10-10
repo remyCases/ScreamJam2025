5ed3f53ccddfada141372c7de410aeada451b64a
remyCases <remy.cases@gmail.com>
2025-10-10 16:08:39 +0200

    [Minor] first iteration of bubbles using helix movement

--- Scripts/game_variables.gd
+++ Scripts/game_variables.gd
@@ -1,1 +1,9 @@
 extends Node
+
+var time_engine_start: int
+
+func _ready() -> void:
+	time_engine_start = Time.get_ticks_msec()
+
+func get_time_ms() -> int:
+	return Time.get_ticks_msec() - time_engine_start
