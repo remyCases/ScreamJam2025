e3e6ee6b88e98a0d349a7ce2940b575b915c826f
remyCases <remy.cases@gmail.com>
2025-10-17 01:50:47 +0200

    [Minor] adding area around diving bell

--- Scripts/collectible_controller.gd
+++ Scripts/collectible_controller.gd
@@ -34,7 +34,7 @@ func create_cylinder_area(area: Area3D, radius: float):
 	var shape = CollisionShape3D.new()
 	shape.shape = CylinderShape3D.new()
 	shape.shape.radius = radius
-	shape.shape.height = 4.0
+	shape.shape.height = 10.0
 	area.add_child(shape)
 
 	# collision layer as collectibles
